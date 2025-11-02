f23db07fe628620bb86a2af7c05c5db412a9ca30
sadhnasrinivasan2006-gif <sadhnasrinivasan2006@gmail.com>
2025-11-02 09:06:31 +0530

    calculator.java

--- calculator.java
+++ calculator.java
@@ -59,3 +59,101 @@ class calculator extends JFrame implements ActionListener {
      b9.addActionListener(c);
      b8.addActionListener(c);
      b7.addActionListener(c);
+     b6.addActionListener(c);
+     b5.addActionListener(c);
+     b4.addActionListener(c);
+     b3.addActionListener(c);
+     b2.addActionListener(c);
+     b1.addActionListener(c);
+     b0.addActionListener(c);
+     be.addActionListener(c);
+     beq.addActionListener(c);
+     beq1.addActionListener(c);
+
+     p.add(l);
+     p.add(ba);
+     p.add(b1);
+     p.add(b2);
+     p.add(b3);
+     p.add(bs);
+     p.add(b4);
+     p.add(b5);
+     p.add(b6);
+     p.add(bm);
+     p.add(b7);
+     p.add(b8);
+     p.add(b9);
+     p.add(bd);
+     p.add(be);
+     p.add(b0);
+     p.add(beq);
+     p.add(beq1);
+
+     p.setBackground(Color.blue);
+
+     f.add(p);
+
+     f.setSize(200, 220);
+     f.show();
+ }
+ public void actionPerformed(ActionEvent e)
+ {
+     String s = e.getActionCommand();
+
+     if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
+         if (!s1.equals(""))
+             s2 = s2 + s;
+         else
+             s0 = s0 + s;
+
+         l.setText(s0 + s1 + s2);
+     }
+     else if (s.charAt(0) == 'C') {
+         s0 = s1 = s2 = "";
+
+         l.setText(s0 + s1 + s2);
+     }
+     else if (s.charAt(0) == '=') {
+
+         double te;
+
+         if (s1.equals("+"))
+             te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+         else if (s1.equals("-"))
+             te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+         else if (s1.equals("/"))
+             te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+         else
+             te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+
+         l.setText(s0 + s1 + s2 + "=" + te);
+
+         s0 = Double.toString(te);
+
+         s1 = s2 = "";
+     }
+     else {
+         if (s1.equals("") || s2.equals(""))
+             s1 = s;
+         else {
+             double te;
+
+             if (s1.equals("+"))
+                 te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+             else if (s1.equals("-"))
+                 te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+             else if (s1.equals("/"))
+                 te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+             else
+                 te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+
+             s0 = Double.toString(te);
+             s1 = s;
+
+             s2 = "";
+         }
+
+         l.setText(s0 + s1 + s2);
+     }
+ }
+}
